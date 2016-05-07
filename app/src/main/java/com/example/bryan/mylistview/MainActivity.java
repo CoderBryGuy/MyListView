76d5a8b92cf71591eaad9c62dabc3239961059d4
CoderBryGuy <bryan.dov.bergman@gmail.com>
2016-05-07 21:46:38 +0300

    kinda of working, kind of not working

--- app/src/main/java/com/example/bryan/mylistview/MainActivity.java
+++ app/src/main/java/com/example/bryan/mylistview/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.bryan.mylistview;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
--- app/src/main/java/com/example/bryan/mylistview/MyView.java
+++ app/src/main/java/com/example/bryan/mylistview/MyView.java
@@ -1,8 +1,12 @@
 package com.example.bryan.mylistview;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.nfc.Tag;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -12,40 +16,166 @@ import android.widget.ScrollView;
  */
 public class MyView extends ViewGroup {
 
-    private ScrollView scrollView;
+    //private ScrollView scrollView;
     private LinearLayout linearLayout;
     private ImageView imageView;
 
     private Context context;
 
+    private int deviceWidth;
+    private final static int STAR_COUNT = 5;
+
     public MyView(Context context) {
         super(context);
         this.context = context;
+        init(this.context);
 
         linearLayout = new LinearLayout(this.context);
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < STAR_COUNT; i++) {
             imageView = new ImageView(this.context);
             imageView.setImageResource(R.drawable.star_checkbox_unchecked1);
+            imageView.setTag(i+1);
+           // Tag myTag = (Tag)imageView.getTag();
+            MyOnClickListener myOnClickListener = new MyOnClickListener(this.context, i+1 , linearLayout);
+            imageView.setOnClickListener(myOnClickListener);
             linearLayout.addView(imageView);
         }
+        this.addView(linearLayout);
 
-        scrollView = new ScrollView(this.context);
-        scrollView.addView(linearLayout);
-        scrollView.layout(0,0,linearLayout.getMeasuredWidth(), linearLayout.getMeasuredHeight());
 
+
+        //scrollView = new ScrollView(this.context);
+       // scrollView.addView(linearLayout);
+        //scrollView.layout(0,0,linearLayout.getMeasuredWidth(), linearLayout.getMeasuredHeight());
+        //this.addView(scrollView);
        }//MyView()constructor
+
+    private void init(Context context) {
+        final Display display = ((WindowManager) context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        Point deviceDisplay = new Point();
+        display.getSize(deviceDisplay);
+        deviceWidth = deviceDisplay.x;
+    }
+    public static class MyOnClickListener implements View.OnClickListener{
+
+        private Context context;
+        private int tagKey;
+        private LinearLayout linearLayout;
+
+        public MyOnClickListener(Context context, int tagKey, LinearLayout linearLayout){
+            this.context = context;
+            this.tagKey = tagKey;
+            this.linearLayout = linearLayout;
+        }
+        @Override
+        public void onClick(View v) {
+
+
+            for (int i = 0; i < STAR_COUNT; i++) {
+             //ImageView imageView = (ImageView)v.findViewWithTag(i+1);
+             //ImageView imageView = (ImageView)v;
+              // imageView.setVisibility(View.INVISIBLE);
+                //imageView.setImageBitmap(null);
+             }
+
+            for (int i = 0; i < tagKey; i++) {
+               ImageView imageView = new ImageView(context);
+                imageView.setImageResource(R.drawable.star_checkbox_checked1);
+                imageView.setTag(i+1);
+                linearLayout.addView(imageView);
+                }
+            for (int i = 0; i < STAR_COUNT - tagKey; i++) {
+                ImageView imageView = new ImageView(context);
+                imageView.setImageResource(R.drawable.star_checkbox_unchecked1);
+                imageView.setTag(i+1);
+                linearLayout.addView(imageView);
+            }
+        }
+    }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         final int count = getChildCount();
+        int curWidth, curHeight, curLeft, curTop, maxHeight;
+
+        //get the available size of child view
+        final int childLeft = this.getPaddingLeft();
+        final int childTop = this.getPaddingTop();
+        final int childRight = this.getMeasuredWidth() - this.getPaddingRight();
+        final int childBottom = this.getMeasuredHeight() - this.getPaddingBottom();
+        final int childWidth = childRight - childLeft;
+        final int childHeight = childBottom - childTop;
+
+        maxHeight = 0;
+        curLeft = childLeft;
+        curTop = childTop;
+
         for (int i = 0; i < count; i++) {
-            //View child = getChildAt(i);
-            getChildAt(i).layout(l, t, r, b);
-        //      getChildAt(i).layout((int)child.getX(), (int)(child.getY()
-            // + child.getMeasuredHeight()), (int) (child.getX()
-            // + child.getMeasuredWidth()), (int)child.getY());
+            View child = getChildAt(i);
+
+            if (child.getVisibility() == GONE)
+                return;
+
+            //Get the maximum size of the child
+            child.measure(MeasureSpec.makeMeasureSpec(childWidth, MeasureSpec.AT_MOST), MeasureSpec.makeMeasureSpec(childHeight, MeasureSpec.AT_MOST));
+            curWidth = child.getMeasuredWidth();
+            curHeight = child.getMeasuredHeight();
+            //wrap is reach to the end
+            if (curLeft + curWidth >= childRight) {
+                curLeft = childLeft;
+                curTop += maxHeight;
+                maxHeight = 0;
+            }
+            //do the layout
+            child.layout(curLeft, curTop, curLeft + curWidth, curTop + curHeight);
+            //store the max height
+            if (maxHeight < curHeight)
+                maxHeight = curHeight;
+            curLeft += curWidth;
         }
     }//onLayout()
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count = getChildCount();
+        // Measurement will ultimately be computing these values.
+        int maxHeight = 0;
+        int maxWidth = 0;
+        int childState = 0;
+        int mLeftWidth = 0;
+        int rowCount = 0;
+
+        // Iterate through all children, measuring them and computing our dimensions
+        // from their size.
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+
+            if (child.getVisibility() == GONE)
+                continue;
+
+            // Measure the child.
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            maxWidth += Math.max(maxWidth, child.getMeasuredWidth());
+            mLeftWidth += child.getMeasuredWidth();
+
+            if ((mLeftWidth / deviceWidth) > rowCount) {
+                maxHeight += child.getMeasuredHeight();
+                rowCount++;
+            } else {
+                maxHeight = Math.max(maxHeight, child.getMeasuredHeight());
+            }
+            childState = combineMeasuredStates(childState, child.getMeasuredState());
+        }
+
+        // Check against our minimum height and width
+        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
+        maxWidth = Math.max(maxWidth, getSuggestedMinimumWidth());
+
+        // Report our final dimensions.
+        setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, childState),
+                resolveSizeAndState(maxHeight, heightMeasureSpec, childState << MEASURED_HEIGHT_STATE_SHIFT));
+
+}//onMeasure()
+
 }//end MyView() class
